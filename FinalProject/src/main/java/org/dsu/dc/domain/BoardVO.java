c2332c0e9c2031e9f6c213e8c8b6f6b67473fc8e
3hagnyeon <3hagnyeon@117.17.40.176>
2019-12-23 14:02:55 +0900

    fjdskfsdlf

---
+++ FinalProject/src/main/java/org/dsu/dc/domain/BoardVO.java
@@ -0,0 +1,27 @@
+package org.dsu.dc.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private long bno;
+	private long STUDNO;
+	private String NAME;
+	private long PCNO;
+	private Date STARTTIME;
+	private Date FINISHTIME;
+	
+	public String getUseTime() {
+		long start = STARTTIME.getTime();
+		long end = FINISHTIME.getTime();
+		
+		long use = end - start;
+		int sec = (int) (use / 1000) % 60;
+		int min = (int) (use / (1000 * 60) % 60);
+		int hours = (int) (use / (1000 * 60 * 60) % 24);
+		String usetime = (hours + "시 " + min + "분 " + sec + "초");
+		return usetime;
+	}
+}
